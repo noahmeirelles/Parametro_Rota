a72d65b42be9c0cf5cabc770acbb0e183ef31e47
Noah Zavickis <noahzavickis@aluno.senai.br>
2024-03-13 15:28:10 -0300

    feate: create profile screen

---
+++ src/components/Title/styles.js
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 20,
+        fontWeight: 'bold',
+    }});
+    export default styles;
---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import styles from './styles'
+import Title from '../../components/Title'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Title title={"eu soumagavana"}/>
+    </View>
+  )
+}
+
+export default Home
